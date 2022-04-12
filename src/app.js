48a99a1ee416a02e99a8bc42699f3591c46a5f38
AntoNunez <a.nunez.jeria@gmail.com>
2022-04-12 19:57:23 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello World");
 };
+
+let pronom = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dominio = [".com", ".net", ".us", ".io", ".uk", ".es"];
+
+function domain(pronom, adj, noun, dominio) {
+  for (let a = 0; a < pronom.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < dominio.length; d++) {
+          console.log("www." + pronom[a] + adj[b] + noun[c] + dominio[d]);
+        }
+      }
+    }
+  }
+}
+domain(pronom, adj, noun, dominio);
